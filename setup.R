0b1985a6d248c6ac0f68e39a7dbd5af908014a6f
Stefan Coors <stefan.coors@gmx.net>
2021-06-24 16:59:10 +0200

    extended pipeline for all learners

--- setup.R
+++ setup.R
@@ -19,9 +19,9 @@ SCORE_MEASURES = c(
 
 # Learners
 LEARNER_IDS = c(
-  # "classif.cv_glmnet",
+  "classif.cv_glmnet",
   # "classif.ranger",
-  # "classif.gamboost", # multiclass pipeline für gamboost
+  "classif.gamboost", # multiclass pipeline für gamboost
   "classif.autocompboost",
   "classif.autocompboost.with_trees"
 )
@@ -31,10 +31,10 @@ LEARNER_IDS = c(
 
 
 # Tuning
-TUNING = FALSE
-TUNING_BUDGET = 3600L * 3
+# TUNING = FALSE
+TUNING_BUDGET = 3600L * 1
 RESAMPLING_INNER = rsmp("cv", folds = 5L)
-TUNER = tnr("intermbo") #tnr("hyperband", eta = 2L)
+TUNER = tnr("random_search") #tnr("hyperband", eta = 2L)
 TERMINATOR = trm("run_time", secs = TUNING_BUDGET) #trm("none")
 
 
@@ -42,15 +42,74 @@ TERMINATOR = trm("run_time", secs = TUNING_BUDGET) #trm("none")
   # if (learner_id =
 # }
 
+autocompboost_preproc_pipeline = function(task, max_cardinality = 100) {
+  has_type_feats = function(types, if_null = TRUE) {
+    if (is.null(task)) if_null else any(types %in% task$feature_types$type)
+  }
+
+  if (!is.null(task)) assert_task(task)
+
+  pos = list()
+  pos = c(pos, po("removeconstants"))
+
+  if (has_type_feats("character")) {
+    pos = c(pos, po("colapply", id = "char_to_fct", param_vals = list(affect_columns = selector_type("character"), applicator = function(x) as.factor(x))))
+  }
+
+  if (has_type_feats("logical")) {
+    pos = c(pos, po("colapply", id = "lgl_to_fct", param_vals = list(affect_columns = selector_type("logical"), applicator = function(x) as.factor(x))))
+  }
+
+  # if (has_type_feats("integer")) {
+    # pos = c(pos, po("colapply", id = "int_to_dbl", param_vals = list(affect_columns = selector_type("integer"), applicator = function(x) as.numeric(x))))
+  # }
+
+  if (has_type_feats("POSIXct")) {
+    pos = c(pos, po("datefeatures", param_vals = list(affect_columns = selector_type("POSIXct"))))
+  }
+
+  if (sum(task$missings()) > 0 && has_type_feats(c("numeric", "integer"))) {
+    pos = c(pos,
+      gunion(list(
+        po("imputehist"),
+        po("missind", param_vals = list(affect_columns = selector_type(c("numeric", "integer")), type = "factor")))) %>>%
+      po("featureunion"))
+  }
+
+  # Impute factors
+  if (sum(task$missings()) > 0 && has_type_feats(c("factor", "ordered", "character"))) {
+    pos = c(pos, po("imputemode"))
+  }
+
+  # Fix extra factor levels
+  if (has_type_feats(c("factor", "ordered"))) {
+    pos = c(pos, po("fixfactors"))
+  }
+
+  # Ensure all factor levels are encoded during predict FIXME: should we sample those or drop features with NA ratio above x%?
+  if (sum(task$missings()) > 0 && has_type_feats(c("factor", "ordered", "character"))) {
+    pos = c(pos, po("imputesample", affect_columns = selector_type(c("factor", "ordered", "character"))))
+  }
+
+  # Collapse factors over 1000 levels
+  # FIXME: Can be improved after #330 is solved
+  if (is.null(task)) {
+    pos = c(pos, po("collapsefactors", param_vals = list(target_level_count = max_cardinality)))
+  } else {
+    if (any(mlr3misc::map_lgl(task$levels(task$feature_types$id[task$feature_types$type == "factor"]), function(x) length(x) > max_cardinality))) {
+      pos = c(pos, po("collapsefactors", param_vals = list(target_level_count = max_cardinality)))
+    }
+  }
+  pos = c(pos, po("removeconstants", id = "removeconstants_end"))
+
+  as_graph(Reduce(`%>>%`, pos))
+}
 
 # helper functions
 # graphlearner
 getGraphLearner = function(learner_id, task) {
-  pipeline = po("removeconstants") %>>%
-    # po("extremwerte_raus") 3*sd %>>%
-    po("imputesample") %>>%
-    po("fixfactors") %>>%
-    po("encode")
+  pipeline = autocompboost_preproc_pipeline(task) %>>%
+    po("encode", method = "treatment")
 
   if (learner_id == "classif.gamboost" && length(task$class_names) > 2) {
     as_learner(
@@ -72,18 +131,18 @@ getFinalLearner = function(learner_id, task) {
     m = msr("classif.logloss")
   }
   if (learner_id == "classif.autocompboost") {
-    return(AutoCompBoost(
+    at = AutoCompBoost(
       task = task,
-      tuning_time = TUNING_BUDGET,
+      tuning_method = "hyperband",
       measure = m,
-      enable_tuning = TUNING
-    )$learner)
+    )$learner
+    at$id = learner_id
+    return(at)
   } else if (learner_id == "classif.autocompboost.with_trees") {
     at = AutoCompBoost(
       task = task,
       param_values = list(add_deeper_interactions = TRUE),
-      tuning_time = TUNING_BUDGET,
-      enable_tuning = TUNING,
+      tuning_method = "hyperband",
       measure = m
     )$learner
     at$id = learner_id
@@ -91,10 +150,10 @@ getFinalLearner = function(learner_id, task) {
   } else {
     gl = getGraphLearner(learner_id, task)
     if (learner_id == "classif.ranger") {
+      gl$id = learner_id
       return(gl)
     } else {
-      return(
-        AutoTuner$new(
+      at =  AutoTuner$new(
           learner = gl,
           search_space = getTuningParams(learner_id, task),
           resampling = RESAMPLING_INNER,
@@ -102,7 +161,8 @@ getFinalLearner = function(learner_id, task) {
           terminator = TERMINATOR,
           measure = msr(ifelse(length(task$class_names) == 2, "classif.auc", "classif.logloss"))
         )
-      )
+      at$id = learner_id
+      return(at)
     }
   }
 }
